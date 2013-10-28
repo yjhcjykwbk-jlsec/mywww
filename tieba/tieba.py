f4ad01352e61a3ecb11924a9e01d765f19413457
U-SAP_ALL\I303035 <I303035@SZXN50832849A.sap.corp>
2013-10-28 23:27:53 +0800

    ..

--- tieba/tieba.py
+++ tieba/tieba.py
@@ -6,22 +6,32 @@ from bs4 import BeautifulSoup as bs
 from pyquery import PyQuery as pyq
 import urllib
 import MySQLdb as sql
-fp=open('test.html')
-soup=bs(fp)
-print soup.find_all("div class=\"p_content\"")
+fp=open('test2.html')
+#soup=bs(fp)
+#print soup.find_all("div class=\"p_content\"")
+
+
+def myprint(content):
+  print type(content)
+  print content
 
 class Tieba:
   def __init__(self):
     self.db="db"
   def insertPost(self,post_id, post_content):
-    print "insert into table(postid,postcontent) values ('%s','%s')"%(post_id,post_content)
+    sql="insert into posts(postid,postcontent) values ('%s',%s)"%(post_id,post_content)
+    print sql
+    if cursor.execute(sql)!=1:
+      print "FAILED"
     print
 # database
 db = sql.connect("127.0.0.1","root","","tieba" )
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
-doc=pyq(url="http://tieba.baidu.com/p/2072174673?pid=31948424950&cid=0#31948424950")
+#html=fp.read();#gbk 
+html=pyq(url="http://tieba.baidu.com/p/2072174673?pid=31948424950&cid=0#31948424950")
+doc=pyq(html)
 d_post_contents=doc('.d_post_content')
 tieba=Tieba()
 for d_post_content in d_post_contents:
@@ -30,12 +40,14 @@ for d_post_content in d_post_contents:
   post_id=post_content_id.split('_')[2]
   post_content=pyq(d_post_content).text()
   print post_id 
-  print "########"
-  print post_content
   try:
-    post_content=sql.escape_string(str(post_content))
-    print post_content
-    tieba.insertPost(post_id,post_content)
+    post_content=post_content.replace("'","\\'");
+    post_content=post_content.decode("utf-8").encode("gbk");
+    myprint(post_content);
+    tieba.insertPost(post_id,"'"+post_content+"'")
+    #post_content=repr(post_content);
+    #myprint(post_content)
+    #tieba.insertPost(post_id,post_content)
   except Exception as err: 
     print "ERROR:"
     print err
@@ -43,16 +55,19 @@ for d_post_content in d_post_contents:
     print 
 
 try:
-  str="a&nbspb"
-  print sql.escape_string(str)
-  tieba.insertPost(12,1);
+  str="中国"
+  #print sql.escape_string(str)
 except Exception as err:
   print err
 
-try:
-  str="a b"
-  str=sql.escape_string(str)
-  tieba.insertPost(12,str);
-except Exception as err:
-  print err
 
+
+
+
+
+
+
+
+
+
+    #post_content=sql.escape_string(str(post_content))
