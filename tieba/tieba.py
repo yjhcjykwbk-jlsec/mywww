1cae703c3997b32205a3103a5ff7a0c355830f4e
U-SAP_ALL\I303035 <I303035@SZXN50832849A.sap.corp>
2013-10-28 21:58:19 +0800

    ..

--- tieba/tieba.py
+++ tieba/tieba.py
@@ -1,29 +1,23 @@
 #encoding=utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
 from bs4 import BeautifulSoup as bs
 from pyquery import PyQuery as pyq
 import urllib
-import MySQLdb
+import MySQLdb as sql
 fp=open('test.html')
-demoHtml = """
-<html>
-<body>
-<div class="icon_col">
-        <h1 class="h1user">中国crifan</h1>
- </div>
- </body>
-</html>
-""";
 soup=bs(fp)
 print soup.find_all("div class=\"p_content\"")
 
 class Tieba:
   def __init__(self):
     self.db="db"
-  def insertPost(post_id,post_content):
-    print "insert into table(postid,postcontent) values (%,%)"%(post_id,post_content)
-
+  def insertPost(self,post_id, post_content):
+    print "insert into table(postid,postcontent) values ('%s','%s')"%(post_id,post_content)
+    print
 # database
-db = MySQLdb.connect("127.0.0.1","root","","tieba" )
+db = sql.connect("127.0.0.1","root","","tieba" )
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
@@ -37,7 +31,28 @@ for d_post_content in d_post_contents:
   post_content=pyq(d_post_content).text()
   print post_id 
   print "########"
-  post_content=urllib.quote(post_content)
   print post_content
-  print 
-  tieba.insertPost(post_id,post_content)
+  try:
+    post_content=sql.escape_string(str(post_content))
+    print post_content
+    tieba.insertPost(post_id,post_content)
+  except Exception as err: 
+    print "ERROR:"
+    print err
+  finally: 
+    print 
+
+try:
+  str="a&nbspb"
+  print sql.escape_string(str)
+  tieba.insertPost(12,1);
+except Exception as err:
+  print err
+
+try:
+  str="a b"
+  str=sql.escape_string(str)
+  tieba.insertPost(12,str);
+except Exception as err:
+  print err
+
