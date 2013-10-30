cdebccc01bb233de3f0f9353149e713ee6a57cf2
U-SAP_ALL\I303035 <I303035@SZXN50832849A.sap.corp>
2013-10-30 14:48:24 +0800

    ..

--- tieba/tieba.sql
+++ tieba/tieba.sql
@@ -1,21 +1,29 @@
+-- phpMyAdmin SQL Dump
+-- version 3.1.3.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 30, 2013 at 06:47 AM
+-- Server version: 5.1.33
+-- PHP Version: 5.2.9-2
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
---  `tieba`
+-- Database: `tieba`
 --
-CREATE DATABASE if not exists `tieba` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE `tieba` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `tieba`;
 
 -- --------------------------------------------------------
 
 --
--- `lzls`
+-- Table structure for table `lzls`
 --
 
 CREATE TABLE IF NOT EXISTS `lzls` (
   `postid` bigint(20) DEFAULT NULL,
-  `spid` bigint(20),
+  `spid` bigint(20) NOT NULL DEFAULT '0',
   `content` text,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`spid`)
@@ -24,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `lzls` (
 -- --------------------------------------------------------
 
 --
---  `posts`
+-- Table structure for table `posts`
 --
 
 CREATE TABLE IF NOT EXISTS `posts` (
@@ -38,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
 -- --------------------------------------------------------
 
 --
---  `thread_details`
+-- Table structure for table `thread_details`
 --
 
 CREATE TABLE IF NOT EXISTS `thread_details` (
@@ -51,13 +59,13 @@ CREATE TABLE IF NOT EXISTS `thread_details` (
 -- --------------------------------------------------------
 
 --
---  `threads`
+-- Table structure for table `threads`
 --
 
 CREATE TABLE IF NOT EXISTS `threads` (
   `tid` bigint(20) DEFAULT NULL,
-  `postid` bigint(20) DEFAULT NULL,
-  `timestamp` timestamp,
+  `postid` bigint(20) NOT NULL DEFAULT '0',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`postid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
